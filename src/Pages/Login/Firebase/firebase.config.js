6613fd6189f486c30642c63fb801165e67038f82
Ankitadas2 <dasankitaanu@gmail.com>
2021-11-12 17:51:56 +0530

    config-commit

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -6,14 +6,5 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-
-
-
-    // apiKey: "AIzaSyD46goyx2MQI5nKxTo7LYdv2dtOVgUTYGQ",
-    // authDomain: "sunglass-store-356e5.firebaseapp.com",
-    // projectId: "sunglass-store-356e5",
-    // storageBucket: "sunglass-store-356e5.appspot.com",
-    // messagingSenderId: "851326466322",
-    // appId: "1:851326466322:web:5eb404d51f079b7fe65bcb"
-  };
+};
   export default firebaseConfig
